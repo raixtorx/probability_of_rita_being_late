1433a5d283f5d9a13d952166053fff2353618344
Raiko Torga <raixtorx@gmail.com>
2025-05-30 18:58:37 +0300

    main file

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from probability_of_rita_being_late.probability.probability import generate_lateness_probabilities_seconds
+from probability_of_rita_being_late.plotting.plot import plot_lateness_probabilities
+from probability_of_rita_being_late.utils.time_utils import str_time_to_seconds
+
 def main():
-    print("test")
+    start_time = "08:00"
+    end_time = "09:00"
+    start_sec = str_time_to_seconds(start_time)
+    end_sec = str_time_to_seconds(end_time)
+    
+    # Generate lateness probabilities every 5 minutes (300 seconds)
+    values = generate_lateness_probabilities_seconds(start_sec=start_sec, end_sec=end_sec, step_sec=30)
+    
+    # Plot the probabilities
+    plot_lateness_probabilities(values)
 
 if __name__ == "__main__":
     main()
